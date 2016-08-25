eeaf47f56954e65fc55ec84a412c535e20c491d3
xuxl <xuxl@u-life.com>
2016-08-25 17:39:57 +0800

    user服务提交

---
+++ user-service/user-service-api/src/main/java/com/xuxl/user/service/api/UserService.java
@@ -0,0 +1,37 @@
+package com.xuxl.user.service.api;
+
+import java.util.Date;
+
+import com.xuxl.common.annotation.ApiGroup;
+import com.xuxl.common.annotation.ApiParameter;
+import com.xuxl.common.annotation.HttpApi;
+import com.xuxl.user.service.domain.User;
+
+@ApiGroup(name = "user",owner = "xuxl")
+public interface UserService {
+	
+	/**
+	 * 基本数据类型测试
+	 * @param age
+	 * @return
+	 */
+	@HttpApi(desc = "根据年龄获得用户", name = "user.age")
+	User getUser(@ApiParameter(desc = "年龄", name = "age", required = true) int age);
+	
+	/**
+	 * String类型测试
+	 * @param name
+	 * @return
+	 */
+	@HttpApi(desc = "根据名字获得用户", name = "user.name")
+	User getUser(@ApiParameter(desc = "名字", name = "name", required = true) String name);
+	
+	/**
+	 * 日期类型测试，前端可以给数字
+	 * @param date
+	 * @return
+	 */
+	@HttpApi(desc = "根据生日获得用户", name = "user.birthday")
+	User getUser(@ApiParameter(desc = "生日", name = "birthday", required = true) Date date);
+
+}
