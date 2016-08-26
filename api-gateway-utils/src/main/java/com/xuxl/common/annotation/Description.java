9674a22dd8b65a2db0ed9e91f9a9e1c7e4881f51
xuxiaolu <m18516555764@163.com>
2016-08-26 21:34:02 +0800

    Update Description.java

--- api-gateway-utils/src/main/java/com/xuxl/common/annotation/Description.java
+++ api-gateway-utils/src/main/java/com/xuxl/common/annotation/Description.java
@@ -6,10 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by Ulife on 15/11/28.
- */
-
-/**
  * 用于添加运行时需要用到的注释
  */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.PACKAGE, ElementType.TYPE})
